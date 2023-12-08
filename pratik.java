3e84f927c2e0340e16e22ceba89c85225abf5310
pratzzpatil <pratikpatil.4014@gmail.com>
2023-12-09 02:26:51 +0530

    first commit

---
+++ pratik.java
@@ -0,0 +1,76 @@
+public class pratik
+{
+    public static void main(String[] args)
+    
+    { 
+        // 1. write a java program to add two numbers
+
+        int a=20;
+        int b=30;
+
+        int c=a+b;
+
+        System.out.println(c);
+
+        // 2. write a java program to subtract two number
+
+        double d=33.23;
+        double e=22.15;
+
+        double f=d-e;
+
+        System.out.println(f);
+
+        // 3. write a java program to multiply two numbers
+
+        float g=6.5f;
+        float h=7.5f;
+
+        float i=g*h;
+
+        System.out.println(i);
+
+        // 4. write a java program for division of two numbers
+
+        long j=20000;
+        long k=10000;
+
+        long l=j/k;
+
+        System.out.println(l);
+
+        // 5. write a java program if the given value is true or false
+
+        boolean m=true;
+        boolean n=false;
+
+        boolean o= 3>5;
+
+        System.out.println(o);
+
+        //6. write a java program for the area of the rectangle
+
+        int length=34;
+        int Breadth=56;
+
+        int areaoftherectangle=length*Breadth;
+
+        System.out.println(areaoftherectangle);
+
+        //7. create a java program for string and assign a value to it
+
+        String name="pratik";
+
+        System.out.println(name);
+
+        //8. write a java program to change the value of Mynum from 10 to 30
+
+        int Mynum= 10;
+
+        Mynum=Mynum +20;
+
+        System.out.println(Mynum);
+
+        
+    }
+}
